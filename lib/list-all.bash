be2c76114e033714c1910e703d9f3e4c674096a8
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-07-04 21:57:36 -0500

    Add missing tests for install / Refactor list-all

--- lib/list-all.bash
+++ lib/list-all.bash
@@ -1,15 +1,15 @@
+main() {
+  echo $(list_tags | select_versions)
+}
+
 list_tags() {
   local cmd="curl -s"
   if [ -n "$GITHUB_API_TOKEN" ]; then
     cmd="$cmd -H 'Authorization: token $GITHUB_API_TOKEN'"
   fi
-  $cmd https://api.github.com/repos/commercialhaskell/stack/tags
+  $cmd "https://api.github.com/repos/commercialhaskell/stack/tags"
 }
 
 select_versions() {
   jq -r .[].name | sed s/^v//
-}
-
-main() {
-  echo $(list_tags | select_versions)
 }
