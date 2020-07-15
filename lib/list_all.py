4c0ab77f65f12ebd6db317c1bbbdd998d3944741
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-07-14 21:45:35 -0500

    Make some methods private

--- lib/list_all.py
+++ lib/list_all.py
@@ -1,13 +1,13 @@
 from lib.github_client import GithubClient
 
 def list_all(client, printer=print):
-  tags = list_tags(client)
-  printer(tags_to_versions(tags))
+  tags = __list_tags(client)
+  printer(__tags_to_versions(tags))
 
-def list_tags(client):
+def __list_tags(client):
   return client.list_repo_tags('commercialhaskell', 'stack')
 
-def tags_to_versions(tags):
+def __tags_to_versions(tags):
   names = list(map(lambda tag: tag['name'][1:], tags))
   return ' '.join(names[::-1])
 
