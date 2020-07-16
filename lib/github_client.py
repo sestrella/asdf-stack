e8a8db3a22590d3a7f7a8c9acf4bda6aeaf346f0
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-07-16 10:07:11 -0500

    Refactor list_all tests mocking GH client

--- lib/github_client.py
+++ lib/github_client.py
@@ -1,9 +1,20 @@
 class GithubClient:
-  URL = 'https://api.github.com'
+  BASE_URL = 'https://api.github.com'
 
-  def __init__(self, api_token):
+  def __init__(self, api_token = None):
     self.api_token = api_token
 
   def list_repo_tags(self, owner, repo):
-    with urllib.request.urlopen(f'{URL}/repos/{owner}/{repo}/tags') as response:
+    # TODO: Pass Authorization header
+    url = f'{BASE_URL}/repos/{owner}/{repo}/tags'
+    return __get_request(url)
+
+  def get_release_by_name(self, owner, repo, tag):
+    # TODO: Pass Authorization header
+    url = f'{BASE_URL}/repos/{owner}/{repo}/releases/tags/{tag}'
+    return __get_request(url)
+
+  def __get_request(url):
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
       return json.loads(response.read())
--- test/test_list_all.py
+++ test/test_list_all.py
@@ -1,19 +1,50 @@
 from lib.github_client import GithubClient
 from lib.list_all import list_all
 from unittest.mock import Mock
+from pathlib import Path
 
+import json
 import unittest
 
 class TestListAll(unittest.TestCase):
   def test_list_all(self):
-    client = GithubClient('secret')
-    client.list_repo_tags = Mock(return_value=[{
-      'name': 'v2.3.1'
-    }, {
-      'name': 'v2.3.0.1'
-    }])
+    client = GithubClient()
+    client.list_repo_tags = Mock(return_value=self.__read_json('./test/fixtures/tags.json'))
     printer = Mock()
-
     list_all(client, printer)
+    printer.assert_called_once_with(' '.join([
+      '1.1.2',
+      '1.2.0',
+      '1.3.0',
+      '1.3.2',
+      '1.4.0',
+      '1.5.0-rc1',
+      '1.5.0',
+      '1.5.1',
+      '1.6.0.20171017',
+      '1.6.0.20171022',
+      '1.6.0.20171202',
+      '1.6.1',
+      '1.6.1.1',
+      '1.6.3',
+      '1.6.3.1',
+      '1.6.5',
+      '1.7.0.1',
+      '1.7.0.3',
+      '1.7.1',
+      '1.9.0.1',
+      '1.9.1',
+      '1.9.1.1',
+      '1.9.3',
+      '1.9.3.1',
+      '2.1.0.1',
+      '2.1.0.3',
+      '2.1.1',
+      '2.1.3',
+      '2.3.0.1',
+      '2.3.1'
+  ]))
 
-    printer.assert_called_once_with('2.3.0.1 2.3.1')
+  def __read_json(self, path):
+    with open(path, 'r') as file:
+      return json.loads(file.read())
