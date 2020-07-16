f888ed3911f588c67bc983b506a39e2bb7cf2d1a
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-07-16 10:19:44 -0500

    Pass token to request if it exists

--- lib/github_client.py
+++ lib/github_client.py
@@ -1,20 +1,20 @@
 class GithubClient:
   BASE_URL = 'https://api.github.com'
 
-  def __init__(self, api_token = None):
-    self.api_token = api_token
+  def __init__(self, token = None):
+    self.token = token
 
   def list_repo_tags(self, owner, repo):
-    # TODO: Pass Authorization header
     url = f'{BASE_URL}/repos/{owner}/{repo}/tags'
     return __get_request(url)
 
   def get_release_by_name(self, owner, repo, tag):
-    # TODO: Pass Authorization header
     url = f'{BASE_URL}/repos/{owner}/{repo}/releases/tags/{tag}'
     return __get_request(url)
 
   def __get_request(url):
     request = urllib.request.Request(url)
+    if not self.token:
+      request.headers = { 'Authorization': f'token {self.token}' }
     with urllib.request.urlopen(request) as response:
       return json.loads(response.read())
