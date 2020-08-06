aacca63f6a27fd8f7f5708467a0ae4095b450c52
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-08-05 20:09:55 -0500

    Fix tests

--- lib/install.py
+++ lib/install.py
@@ -1,18 +1,53 @@
-from lib.github_client import GithubClient
-
+import logging
+import os
 import platform
+import shutil
 import sys
+import tarfile
+import tempfile
+import urllib.request
+
+logger = logging.getLogger('install')
 
 def install(client, install_dir, version):
-  urls = __compatible_assets(assets_by_version(client, version))
-  print(list(urls))
-  return None
+  urls = list(__compatible_urls(__downloadable_urls(client, version)))
+  logger.info(f'Available sources {urls}')
+  return __install_static(install_dir, urls) or __install_dynamic(install_dir, urls)
 
-def assets_by_version(client, version):
+def __downloadable_urls(client, version):
   assets = client.get_release_by_name('commercialhaskell', 'stack', f'v{version}')['assets']
   return map(lambda asset: asset['browser_download_url'], assets)
 
-def __compatible_assets(urls):
+def __compatible_urls(urls):
   os = sys.platform
   arch = platform.machine()
-  return filter(lambda url: f'{os}-{arch}' in url, urls)
+  return filter(lambda url: f'{os}-{arch}' in url and url.endswith('tar.gz'), urls)
+
+def __install_static(install_dir, urls):
+  logger.info('Installing static binary')
+  url = next((url for url in urls if 'static' in url), None)
+  return __install_from_url(install_dir, url)
+
+def __install_dynamic(install_dir, urls):
+  logger.info('Installing dynamic binary')
+  url = next((url for url in urls if not 'gmp4' in url), None)
+  return __install_from_url(install_dir, url)
+
+def __install_from_url(install_dir, url):
+  if not url:
+    logger.info('No source available')
+    return False
+
+  with tempfile.TemporaryDirectory() as download_dir:
+    logger.info(f'Downloading {url} to {download_dir}')
+    path, _ = urllib.request.urlretrieve(url, f"{download_dir}/stack.tar.gz")
+    with tarfile.open(path) as tar:
+      tar.extractall(download_dir)
+      dirname = url.rsplit('/', 1)[1].rstrip('tar.gz')
+      os.mkdir(f"{install_dir}/bin")
+      __copy_file(f'{download_dir}/{dirname}/stack', f'{install_dir}/bin/stack')
+      return True
+
+def __copy_file(src, dest):
+  logger.info(f'Copying {src} to {dest}')
+  shutil.copy(src, dest)
